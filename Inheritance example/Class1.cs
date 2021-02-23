c70c0ef3bfe502a8b6aceeba5af24044901f1d9a
Rishabh <rishirishubro@gmail.com>
2021-02-23 17:00:21 +0530

    Constructor understanding....

--- Inheritance example/Class1.cs
+++ Inheritance example/Class1.cs
@@ -4,6 +4,10 @@ namespace Inheritance_example
 {
     class Class1
     {
+        public Class1(int i)
+        {
+            Console.WriteLine("Class 1 constructor is called " + i);
+        }
        public void Test1()
         {
             Console.WriteLine("Method1");
--- Inheritance example/Class2.cs
+++ Inheritance example/Class2.cs
@@ -6,11 +6,26 @@ namespace Inheritance_example
 {
     class Class2 : Class1
     {
+
+        public Class2(int a) : base(a)
+        {
+            // Console.WriteLine("Class 2 Constructor is called "+ a);
+            Console.WriteLine("Class 2 Constructor is called now");
+        }
+        public void Test3()
+        {
+            Console.WriteLine("Method3");
+        }
         static void Main()
         {
-            Class2 c = new Class2();
-            c.Test1();
-            c.Test2();
+            // Object
+            /*   Object obj = new Object();
+               Console.WriteLine(obj.GetType());
+               Class1 p = new Class1();
+               Console.WriteLine(p.GetType());
+               Class2 c = new Class2();
+               Console.WriteLine(c.GetType()); */
+            Class2 c = new Class2(50);
             Console.ReadLine();
         }
     }
