771e3d7bdd1461f24d1352640614deda03897d0f
Rishabh <rishirishubro@gmail.com>
2021-02-23 14:58:03 +0530

    Add project files.

---
+++ Inheritance example/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Inheritance_example
+{
+    class Class1
+    {
+       public void Test1()
+        {
+            Console.WriteLine("Method1");
+        }
+        public void Test2()
+        {
+            Console.WriteLine("Method2");
+        }
+    }
+}
---
+++ Inheritance example/Class2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance_example
+{
+    class Class2 : Class1
+    {
+        static void Main()
+        {
+            Class2 c = new Class2();
+            c.Test1();
+            c.Test2();
+            Console.ReadLine();
+        }
+    }
+}
